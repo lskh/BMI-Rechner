b790167cd4ad557aeabcd1d6b7077fc478b7bd29
Laurens Kils-Hütten <lkh@betola.de>
2016-09-23 23:01:58 +0200

    kleine Ergänzung

--- BMIfunc.js
+++ BMIfunc.js
@@ -5,6 +5,19 @@ function BMIfunc()
 		var BMI = Gewicht / (Groesse * Groesse);
 
 		document.Formular.BMI.value = BMI.toFixed(1) ;
+		if ( BMI < 18 ) {
+		  document.getElementById("interpretation").innerHTML="Untergewicht";
+		} else if ( BMI > 18 && BMI < 25 ) {
+		  document.getElementById("interpretation").innerHTML = "Normalgewicht";
+    } else if ( BMI > 25 && BMI < 30 ) {
+      document.getElementById("interpretation").innerHTML = "Übergewicht";
+    } else if ( BMI > 30 && BMI < 35 ) {
+      document.getElementById("interpretation").innerHTML ="Adipositas I°";
+    } else if ( BMI > 35 && BMI < 40 ) {
+      document.getElementById("interpretation").innerHTML ="Adipositas II°";
+    } else {
+      document.getElementById("interpretation").innerHTML ="Adipositas III°";
+    }
 	}
 
 document.addEventListener('DOMContentLoaded', function() {
